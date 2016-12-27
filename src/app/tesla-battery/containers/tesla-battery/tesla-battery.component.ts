42b9a592f1faebc9286562a1cd2a236e4357eb73
fb920024 <francesco.benedetto263@gmail.com>
2016-12-27 16:55:37 +0100

    Add tesla-battery container

---
+++ src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
@@ -0,0 +1,44 @@
+/**
+ * Created by francesco on 26/12/2016.
+ */
+import { Component, OnInit } from "@angular/core";
+import {FormGroup, FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'tesla-battery',
+  template: `
+    <form class="tesla-battery" formGroup="tesla">
+      <h1>{{title}}</h1>
+      <div class="tesla-battery__notice">
+        <p>
+          The actual amount of range that you experience will vary based 
+          on your particular use conditions. See how particular use conditions 
+          may affect your range in our simulation model.
+        </p>
+        <p>
+          Vehicle range may vary depending on the vehicle configuration, 
+          battery age and condition, driving style and operating, environmental 
+          and climate conditions.
+        </p>
+      </div>
+    </form>
+  `,
+  styleUrls: ['./tesla-battery.component.scss']
+})
+export class TeslaBatteryComponent implements OnInit {
+  title: string = 'Rage per charge';
+  tesla: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.tesla = this.fb.group({
+      config: this.fb.group({
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19
+      })
+    });
+  }
+}
